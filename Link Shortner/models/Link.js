b8937b8c2b70a972e38478c70f6567a26ac07b1e
pujaPatil169 <marssitara6292@gmail.com>
2025-01-31 19:42:58 +0530

    Delete Link Shortner directory

--- Link Shortner/models/Link.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const LinkSchema = new mongoose.Schema({
-    originalUrl: String,
-    shortUrl: String,
-    shortCode: String,
-    qrCode: String,
-    clicks: { type: Number, default: 0 }
-});
-export default mongoose.model('Link', LinkSchema);
